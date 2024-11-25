ffa85d5929c5a07314c5e4941f129488c9b667da
aarongarciacr <aarongarcia4997@gmail.com>
2024-11-25 13:39:52 -0600

    Add dropdown menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { VscAccount } from "react-icons/vsc";
 import * as sessionActions from "../../store/session";
 
-const ProfileButton = ({ user }) => {
+function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+    // if (!showMenu) setShowMenu(true);
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulRef.current && !ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
     <>
-      <button className="profile-button">
+      <button onClick={toggleMenu} className="profile-button">
         <VscAccount
           style={{
             height: "1.8em",
@@ -24,7 +48,7 @@ const ProfileButton = ({ user }) => {
           }}
         />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName} ref={ulRef}>
         <li id="username">{user.username}</li>
         <li id="name">
           {user.firstName} {user.lastName}
@@ -38,6 +62,6 @@ const ProfileButton = ({ user }) => {
       </ul>
     </>
   );
-};
+}
 
 export default ProfileButton;
