dbae518e8b09c1c52d17cb1c6e1f5b78429102e6
aarongarciacr <aarongarcia4997@gmail.com>
2024-12-02 00:02:41 -0600

    Fix nav responsiveness and change logo

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import Logo from "../../assets/favicon.png";
+import BRB from "../../assets/BRB-no-back.png";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
@@ -15,8 +16,8 @@ function Navigation({ isLoaded }) {
         <li id="home-link-container">
           <Link to="/" className="home-link">
             <img src={Logo} className="logo"></img>
-
-            <p className="illBRB">I&apos;ll BRB</p>
+            <img src={BRB} className="logo-letters"></img>
+            {/* <p className="illBRB">I&apos;ll BRB</p> */}
           </Link>
         </li>
         {isLoaded && (
